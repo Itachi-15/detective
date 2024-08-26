3ceb9c522b3e824f5d0c4af54f74f7e08782b809
Itachi-15 <carasuma106@gmail.com>
2024-08-26 21:28:17 +0300

    Add files via upload
    Signed-off-by: Itachi-15 <carasuma106@gmail.com>

---
+++ script.js
@@ -0,0 +1,107 @@
+function co(){
+    var content = document.querySelector('.a');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        
+    } else {
+        content.style.display = 'none';
+        
+    } 
+
+    var dj = document.querySelector('.togglebutton');
+    if (dj.style.display === 'none') {
+        dj.style.display = 'block';
+        
+     } else {
+        dj.style.display = 'none';
+        
+     }
+}
+
+
+function ama(){
+    var content = document.querySelector('.b');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        
+    } else {
+        content.style.display = 'none';
+        
+    } 
+
+    var dj = document.querySelector('.ama');
+    if (dj.style.display === 'none') {
+        dj.style.display = 'block';
+        
+     } else {
+        dj.style.display = 'none';
+        
+     }
+
+}
+
+
+function han(){
+    var content = document.querySelector('.c');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        
+    } else {
+        content.style.display = 'none';
+        
+    } 
+
+    var dj = document.querySelector('.han');
+    if (dj.style.display === 'none') {
+        dj.style.display = 'block';
+        
+     } else {
+        dj.style.display = 'none';
+        
+     }
+
+}
+
+
+function wak(){
+    var content = document.querySelector('.d');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        
+    } else {
+        content.style.display = 'none';
+        
+    } 
+
+    var dj = document.querySelector('.wak');
+    if (dj.style.display === 'none') {
+        dj.style.display = 'block';
+        
+     } else {
+        dj.style.display = 'none';
+        
+     }
+
+}
+
+
+function kat(){
+    var content = document.querySelector('.e');
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        
+    } else {
+        content.style.display = 'none';
+        
+    } 
+
+    var dj = document.querySelector('.kat');
+    if (dj.style.display === 'none') {
+        dj.style.display = 'block';
+        
+     } else {
+        dj.style.display = 'none';
+        
+     }
+
+}
